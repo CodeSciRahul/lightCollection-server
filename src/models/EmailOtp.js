86fe1cf216ff06ef0b2c5b247586a3a2aa821736
CodeSciRahul <rahulkumarkudra2004@gmail.com>
2026-07-12 15:04:22 +0530

    fix auth

--- src/models/EmailOtp.js
+++ src/models/EmailOtp.js
@@ -14,7 +14,7 @@ const emailOtpSchema = new mongoose.Schema(
     },
     purpose: {
       type: String,
-      enum: ["seller_signup", "customer_login", "seller_login", "dashboard_login"],
+      enum: ["seller_signup", "customer_login", "seller_login", "dashboard_login", "admin_login"],
       default: "seller_signup",
     },
     attempts: {
